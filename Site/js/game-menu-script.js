9ead87b49b823554f26ab963506b3c3a4b1c1600
GusicE <e.gusic88@gmail.com>
2017-08-17 12:23:57 +0200

    intro

--- Site/js/game-menu-script.js
+++ Site/js/game-menu-script.js
@@ -22,3 +22,62 @@ $(".inv_tab").click(function() {
    $(".inv_wrapper[data-inv="+info+"]").addClass(" visible").removeClass("not_visible");
     $(".inv_tab[data-inv="+info+"]").addClass(" active_inv_tab");
 });
+
+var overlay = document.getElementById('welcome_screen');
+var intro1 = document.getElementById('introText_1');
+var intro2 = document.getElementById('introText_2');
+var audio1 = document.getElementById('mainMenuAudio');
+var audio2 = document.getElementById('introAudio');
+	
+function clickStart() {
+    overlay.classList.add("hide");
+    intro1.classList.add("show");
+    intro2.classList.add("show2");
+    audioFadeOut(audio1);
+    audio2.play();
+}
+
+function audioFadeIn(track) {
+var audio = track;    
+var vol = 0.1;
+var interval = 500; // 500ms interval
+    
+audio.volume = vol;    
+
+var fadeout = setInterval(
+  function() {
+    // Reduce volume by 0.05 as long as it is above 0
+    // This works as long as you start with a multiple of 0.05!
+    if (vol < 1) {
+      vol += 0.05;
+      audio.volume = vol;
+    }
+    else {
+      // Stop the setInterval when 0 is reached
+      clearInterval(fadeout);
+    }
+  }, interval);
+};
+
+function audioFadeOut(track) {
+var audio = track;    
+var vol = 1;
+var interval = 100; // 500ms interval
+    
+audio.volume = vol;    
+
+var fadeout = setInterval(
+  function() {
+    // Reduce volume by 0.05 as long as it is above 0
+    // This works as long as you start with a multiple of 0.05!
+    if (vol > 0) {
+      vol -= 0.05;
+      audio.volume = vol;
+    }
+    else {
+      // Stop the setInterval when 0 is reached
+      clearInterval(fadeout);
+      audio.pause();
+    }
+  }, interval);
+};
